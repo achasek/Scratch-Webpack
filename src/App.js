80bdcd542b0bac15804aa46a0985ddba00f26cf0
achasek <chaseknieper@gmail.com>
2023-10-04 12:58:58 -0700

    separated behavior of production and dev mode. Added global variable, or common behavior, to both. Added json-server to act as backend for dev mode, and a 3rd-party url to act as backend for prod mode. Installed axios for our calls; making it incompatible with Internet Explorer. Added custom hook to deal with initializing of data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react' // we need this now also in component files
+import React, { useState, useEffect } from 'react' // we need this now also in component files
+import axios from 'axios'
+import './index.css'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 // by default, our bundler only recognizes regular JS code.
 // the below is not that. It is JSX and is transpiled into JS code through Babel
 // we need to define a Loader in the webpack.config.js to handle this
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
   return (
-    <div className='container'>
-      hello from-scratch webpack
+    <div className="container">
+      hello webpack {counter} clicks
+      <button onClick={handleClick}>
+        press
+      </button>
+      <div>{notes.length} notes on server {BACKEND_URL}</div>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,48 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = () => {
+// CAN ACCESS PROD build locally via CLI command below
+// npx static-server
+// default on http://localhost:9080
+
+const config = (env, argv) => {
+  // we can set webpack to act differently depending on the argv.mode. We can also define constant behavior across all argv.modes
+
+  console.log("argv.mode:", argv.mode)
+  console.log("argv obj:", argv)
+  console.log("env:", env)
+  // consoles the below commented out code
+  // argv.mode: production
+  // argv obj: {
+  //   mode: 'production',
+  //   env: { WEBPACK_BUNDLE: true, WEBPACK_BUILD: true }
+  // }
+  // env: { WEBPACK_BUNDLE: true, WEBPACK_BUILD: true }
+
+  // setting a global variable, or constant behavior, accessible by dev, test, or production mode
+  const backend_url = argv.mode === 'production'
+  ? 'https://notes2023.fly.dev/api/notes'
+  : 'http://localhost:3001/notes'
+
   return {
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'main.js'
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      // allows for app to be ran on localhost:3000
+      port: 3000,
+    },
+    // this allows you to use debuggers and to see where the bug is in your actual pre-bundled code, not the bundled and unreadable code
+    devtool: 'source-map',
+    // fixed a warning about production build exceeding size limitations. Limits source maps to dev mode only
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
     },
     module: {
         rules: [
@@ -22,6 +59,11 @@ const config = () => {
           },
         ],
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
   }
 }
 
